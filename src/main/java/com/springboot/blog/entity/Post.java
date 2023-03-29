4723000c02a9ecd1ed4ddaca307501090bdd4031
Mandy Priscilla <89837607+mandypriscilla@users.noreply.github.com>
2023-03-29 15:50:20 +0700

    Add files via upload

---
+++ src/main/java/com/springboot/blog/entity/Post.java
@@ -0,0 +1,21 @@
+package com.springboot.blog.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "posts", uniqueConstraints = {@UniqueConstraint(columnNames = {"title"})})
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+}
